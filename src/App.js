efc25f25be6172e90dc70bddd4b584914cf585b1
Anthony Hernandez <tony91139@gmail.com>
2018-05-23 21:34:35 -0700

    Add initial App files necessary from create-react-app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <header id="App-Header">
+          <h1 id="App-Title">
+            Omnidraft
+          </h1>
+        </header>
+
+        <div id="App-Body">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
